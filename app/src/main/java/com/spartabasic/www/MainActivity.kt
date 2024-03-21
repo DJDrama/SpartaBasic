fe7216cca0305718e80af0f23ef1dbbb4f295495
DJDrama <putzzang@naver.com>
2024-03-21 13:57:33 +0900

    [Feature] 1st week answer
    (Modified)
    MainActivity.kt
    activity_main.xml

--- app/src/main/java/com/spartabasic/www/MainActivity.kt
+++ app/src/main/java/com/spartabasic/www/MainActivity.kt
@@ -3,13 +3,19 @@ package com.spartabasic.www
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
+    private var job: Job? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,21 +25,53 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupButton()
+        setRandomValueBetweenOneToHundred()
+        setJobAndLaunch()
+    }
 
-        // val = value
-        // var = variable
-        val textView = findViewById<TextView>(R.id.spartaTextView)
+    private fun setupButton() {
         val button = findViewById<Button>(R.id.clickButton)
-
         button.setOnClickListener {
-            textView.text = "Welcome to basic class!"
+            job?.cancel()
+            checkAnswerAndShowToast()
         }
+    }
 
-        var i=0
-        while(i<100){
-            //textView.text = "$i"
-            textView.text = i.toString()
-            i+=1 // ++i, i++
+    private fun setRandomValueBetweenOneToHundred() {
+        val randomTextView = findViewById<TextView>(R.id.textViewRandom)
+        val randomValue = (1..100).random()
+        randomTextView.text = randomValue.toString()
+    }
+
+    private fun setJobAndLaunch() {
+        val textView = findViewById<TextView>(R.id.spartaTextView)
+        job = lifecycleScope.launch {
+/*            var i = 1
+            while (isActive && i <= 100) {
+                textView.text = i.toString()
+                delay(100)
+                i += 1 // ++i, i++
+            }
+            */
+            for (j in 1..100) {
+                if (isActive) {
+                    textView.text = j.toString()
+                    delay(500)
+                }
+            }
+        }
+    }
+
+    private fun checkAnswerAndShowToast() {
+        val textView = findViewById<TextView>(R.id.spartaTextView)
+        val randomTextView = findViewById<TextView>(R.id.textViewRandom)
+
+        val answer = textView.text
+        if (answer == randomTextView.text) {
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show()
         }
     }
 }

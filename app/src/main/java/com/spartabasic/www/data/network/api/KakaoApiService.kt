4207b97f3883f3bc10c186d0721b1d8ab20116a9
DJDrama <putzzang@naver.com>
2024-05-16 10:24:15 +0900

    Retrofit additional info

--- app/src/main/java/com/spartabasic/www/data/network/api/KakaoApiService.kt
+++ app/src/main/java/com/spartabasic/www/data/network/api/KakaoApiService.kt
@@ -3,6 +3,7 @@ package com.spartabasic.www.data.network.api
 import com.spartabasic.www.BuildConfig
 import com.spartabasic.www.data.network.model.ImageSearchDto
 import com.spartabasic.www.data.network.model.VideoSearchDto
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -19,11 +20,11 @@ interface KakaoApiService {
     ): Response<KakaoApiResponse<List<ImageSearchDto>>>
 
     @GET("v2/search/vclip")
-    suspend fun getSearchedVideos(
+    fun getSearchedVideos(
         @Header("Authorization") header: String = "KakaoAK ${BuildConfig.REST_API_KEY}",
         @Query("query") query: String,
         @Query("sort") sort: String,
         @Query("page") page: Int,
         @Query("size") size: Int,
-    ): Response<KakaoApiResponse<List<VideoSearchDto>>>
+    ): Call<KakaoApiResponse<List<VideoSearchDto>>>
 }
